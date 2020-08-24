9dc5aa60eaac20f976681130eb0876a7aef0477f
LeakyCauldron <cwangscholar@gmail.com>
2020-08-24 22:24:28 +0800

    Update __init__.py
    fix bug

--- gym-uav/gym_uav/__init__.py
+++ gym-uav/gym_uav/__init__.py
@@ -2,18 +2,18 @@ from gym.envs.registration import register
 
 register(
     id='uav-v0',
-    entry_point='gym_uav.envs:UavSparseEnv',
+    entry_point='gym_uav.envs:UavEnv',
     max_episode_steps=2000
 )
 
-register(
-    id='uav-v1',
-    entry_point='gym_uav.envs:UavDenseEnv',
-    max_episode_steps=2000
-)
+# register(
+#     id='uav-v1',
+#     entry_point='gym_uav.envs:UavDenseEnv',
+#     max_episode_steps=2000
+# )
 
-register(
-    id='uav-v2',
-    entry_point='gym_uav.envs:UavGoalEnv',
-    max_episode_steps=2000
-)
+# register(
+#     id='uav-v2',
+#     entry_point='gym_uav.envs:UavGoalEnv',
+#     max_episode_steps=2000
+# )
